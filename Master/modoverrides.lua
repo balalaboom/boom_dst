2b27b144c3ac194b3ee596a217b7a0ba7efe3811
Balalaboom <boom@example.com>
2017-12-26 06:00:01 +0000

    26/12/2017 06:00:01

--- Master/modoverrides.lua
+++ Master/modoverrides.lua
@@ -1,6 +1,6 @@
 return {
-  ["workshop-1111711682"]={ configuration_options={  }, enabled=true },
   ["workshop-1242093526"]={ configuration_options={  }, enabled=true },
+  ["workshop-1111711682"]={ configuration_options={  }, enabled=true },
   ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
   ["workshop-378160973"]={
     configuration_options={
