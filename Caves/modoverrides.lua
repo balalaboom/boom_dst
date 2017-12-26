a05159aa17a469f10f0210e61f571a94691a7e40
Balalaboom <boom@example.com>
2017-12-26 10:24:01 +0000

    26/12/2017 10:24:01

--- Caves/modoverrides.lua
+++ Caves/modoverrides.lua
@@ -1,6 +1,6 @@
 return {
   ["workshop-1111711682"]={ configuration_options={  }, enabled=true },
-  ["workshop-1242093526"]={ configuration_options={  }, enabled=true },
+  ["workshop-1242093526"]={ configuration_options={ glowcap=1, mushlight=1, winter_tree=1 }, enabled=true },
   ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
   ["workshop-378160973"]={
     configuration_options={
